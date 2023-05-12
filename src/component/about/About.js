a593ea247bddf29f846e19f9ff214722ec7ea020
MD EBRAHIM <76687323+mdebrahim98@users.noreply.github.com>
2023-05-12 11:22:05 +0530

    Update About.js

--- src/component/about/About.js
+++ src/component/about/About.js
@@ -22,26 +22,28 @@ export default function About() {
           <div className="profile">
             <img src={profile} alt="" height='410px' width='400px' />
           </div>
-          <p>I am pursuing a B.Tech degree in Computer Science and Engineering from MAKAUT, W.B. 
-            I am a Web  and Mern stack developer.I have a passion for learning new technologies by applying my knowledge and skills in a challenging environment. 
-
+          <p>
+            I'm a Web Developer who tries to build things from scratch. I am a highly
+            self-motivated person and I believe in “Work is Worship”. Once I commit
+            myself to a particular task, I make sure I finish it without compromising
+            on the quality.
           </p>
           <h2 className='skill-title'>Key Skills</h2>
           <ul className='skill-list'>
-            <li>Design, Develop and Deploy an app from scratch to One.
-            </li>
+            <li>Design, Develop and Deploy an app from scratch to One.</li>
+            <li>Implementing new features on existing projects.</li>
             <li>I love to learn new technologies if needed.</li>
 
           </ul>
           <h2 className='skill-title'>Technological Skills</h2>
           <ul className='skill-list'>
-            <li>JavaScript</li>
-            <li>Frontend with HTML/CSS, JavaScript, ReactJs</li>
-            <li>Frontend UI with Bootstrap</li>
-            <li>Backend with NodeJS, ExpressJS,</li>
+            <li>JavaScript, ReactJS</li>
+            <li>Front-End with HTML, CSS, Javascript, ReactJS, Redux.js, Next.js</li>
+            <li>Frontend UI with Bootstrap, Material-UI</li>
+            <li>Backend with Node.js, Express.js</li>
             <li>Worked with MySQL, MongoDB</li>
-            <li>c,java,python</li>
-
+            <li>Worked with tools like Git, Github, VS Code, JSON, Markdown</li>
+            <li>Has some knowledge on C, C++, Java, Python</li>
           </ul>
 
           <p>Please find my email id below to connect with me:</p>
@@ -49,7 +51,7 @@ export default function About() {
 
 
         </section>
-        <CopyRight title="Md Ebrahim © 2021" />
+        <CopyRight title=`Md Ebrahim © ${new Date().getFullYear()}` />
       </div>
 
 
